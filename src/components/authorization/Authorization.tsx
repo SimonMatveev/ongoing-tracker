1c1a014298d76eecfcfcfbe4ad0918289ad55718
Gleb Orlovtsev <the.unknown.guest@yandex.com>
2023-11-13 12:34:23 +0300

    Небольшие изменения

--- src/components/authorization/Authorization.tsx
+++ src/components/authorization/Authorization.tsx
@@ -1,18 +1,21 @@
 import './authorization.css';
 import api from '../../utils/shikiApi';
+import { FC } from 'react';
 
-export default function Authorization() {
+const Authorization: FC = () => {
 
-  function handleAuthorize() {
+  const handleAuthorize = () => {
     api.authorize('user_rates+comments+topics')
   }
 
   return (
     <div className="authorization">
       <div className="autorization__container">
-        <h1 className="autorization__title">Привет! Для использования GNEGOMORI<br />необходимо авторизироваться</h1>
+        <h1 className="autorization__title">Привет! Для использования сайта<br />необходимо авторизироваться</h1>
         <button type="button" className="autorization__button" onClick={handleAuthorize}>Авторизация</button>
       </div>
     </div>
   );
 }
+
+export default Authorization;
